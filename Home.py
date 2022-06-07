b390567808b8c0331bcaae352ad51a65be5d2920
Qiusheng Wu <giswqs@gmail.com>
2022-06-06 23:41:16 -0400

    Updated homepage

--- Home.py
+++ Home.py
@@ -11,6 +11,32 @@ Contact: [Dr. Qiusheng Wu](https://wetlands.io)
 st.sidebar.info(markdown)
 st.sidebar.image("https://i.imgur.com/qXqRBda.png")
 
-st.title("TNView")
+st.title("TNView Web App")
 
+st.markdown(
+    """
+    This multi-page web app demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
+    such as [leafmap](https://leafmap.org), [geemap](https://geemap.org), [pydeck](https://deckgl.readthedocs.io), and [kepler.gl](https://docs.kepler.gl/docs/keplergl-jupyter).
+    This is an open-source project and you are very welcome to contribute your comments, questions, resources, and apps as [issues](https://github.com/giswqs/streamlit-tnview/issues) or 
+    [pull requests](https://github.com/giswqs/streamlit-tnview/pulls) to the [GitHub repository](https://github.com/giswqs/streamlit-tnview).
 
+    """
+)
+
+st.info("Click on the left sidebar menu to navigate to the different apps.")
+
+st.subheader("Satellite Timelapses")
+st.markdown(
+    """
+    The following timelapse animations were created using the Timelapse web app. Click `Timelapse` on the left sidebar menu to create your own timelapse for any location around the globe.
+"""
+)
+
+row1_col1, row1_col2 = st.columns(2)
+with row1_col1:
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/spain.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
+
+with row1_col2:
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
